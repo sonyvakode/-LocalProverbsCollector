363c47fede11282bd5c471eae55736402437b537
sonyvakode <sonyvakode648@gmail.com>
2025-07-31 23:12:56 +0530

    Update translate.py

--- utils/translate.py
+++ utils/translate.py
@@ -1,7 +1,2 @@
-def translate_proverb(text, target_lang="hi"):
-    # Dummy translation
-    translations = {
-        "Love conquers all": "प्यार सब पर भारी होता है",
-        "An apple a day": "रोज़ एक सेब डॉक्टर से दूर रखता है"
-    }
-    return translations.get(text, text)
+def translate(text, target_lang_code):
+    return f"[{target_lang_code.upper()}] {text}"
