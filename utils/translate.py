46ad2762154e4cabb80337cb09c6ecf5894404af
sonyvakode <sonyvakode648@gmail.com>
2025-07-31 21:05:55 +0530

    Update translate.py

--- utils/translate.py
+++ utils/translate.py
@@ -1,3 +1,3 @@
 def translate_text(text, target_lang):
-    # Mock translation
+    # Mock translation since we're not using an API
     return f"Translated '{text}' to [{target_lang}] (mock translation)"
