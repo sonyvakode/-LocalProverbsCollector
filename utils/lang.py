cd2823574afa1dd1c5655ab411d8173ce1d83493
sonyvakode <sonyvakode648@gmail.com>
2025-07-31 19:27:14 +0530

    Create lang.py

---
+++ utils/lang.py
@@ -0,0 +1,5 @@
+def get_supported_languages():
+    return [
+        "en", "hi", "ta", "te", "ml", "kn", "mr", "bn", "gu", "pa", "ur",
+        "or", "as", "ne", "sd", "si", "kok", "ks", "doi", "mai", "bho", "sa"
+    ]
