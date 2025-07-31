b274b5b073501bc9c0121d7ee2a8290175a6797d
sonyvakode <sonyvakode648@gmail.com>
2025-07-31 20:28:51 +0530

    Update vote.py

--- utils/vote.py
+++ utils/vote.py
@@ -1,8 +1,11 @@
-import random
 from utils.core import load_proverbs
+import json
 
-def get_random():
-    data = load_proverbs()
-    if not data:
-        return None
-    return random.choice(data).get("proverb", None)
+PROVERB_FILE = "data/proverbs.json"
+
+def cast_vote(index, upvote=True):
+    proverbs = load_proverbs()
+    if 0 <= index < len(proverbs):
+        proverbs[index]["votes"] = proverbs[index].get("votes", 0) + (1 if upvote else -1)
+        with open(PROVERB_FILE, "w", encoding="utf-8") as f:
+            json.dump(proverbs, f, ensure_ascii=False, indent=2)
