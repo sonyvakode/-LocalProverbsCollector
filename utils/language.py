db4c82771842334231b6c29de263e5a5336105c1
sonyvakode <sonyvakode648@gmail.com>
2025-07-31 20:29:14 +0530

    Update language.py

--- utils/language.py
+++ utils/language.py
@@ -1,4 +1,9 @@
-def get_supported_languages():
+def regions():
     return [
-        "en", "hi", "ta", "te", "ml", "kn", "gu", "mr", "bn", "pa", "ur"
+        "Andhra Pradesh", "Assam", "Bihar", "Chhattisgarh", "Goa", "Gujarat",
+        "Haryana", "Himachal Pradesh", "Jharkhand", "Karnataka", "Kerala",
+        "Madhya Pradesh", "Maharashtra", "Manipur", "Meghalaya", "Mizoram",
+        "Nagaland", "Odisha", "Punjab", "Rajasthan", "Sikkim", "Tamil Nadu",
+        "Telangana", "Tripura", "Uttar Pradesh", "Uttarakhand", "West Bengal",
+        "Jammu & Kashmir", "Ladakh"
     ]
