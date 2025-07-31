2e13aa4b517b5ad28948c546b8fd5a0406b3bed9
sonyvakode <sonyvakode648@gmail.com>
2025-07-31 19:36:44 +0530

    Update vote.py

--- utils/vote.py
+++ utils/vote.py
@@ -1,11 +1,11 @@
-import json
 from utils.core import load_proverbs
+import json
 
-DATA_FILE = "data/proverbs.json"
+PROVERB_FILE = "data/proverbs.json"
 
 def record_vote(index):
-    proverbs = load_proverbs()
-    if 0 <= index < len(proverbs):
-        proverbs[index]["votes"] = proverbs[index].get("votes", 0) + 1
-        with open(DATA_FILE, "w", encoding="utf-8") as f:
-            json.dump(proverbs, f, ensure_ascii=False, indent=2)
+    data = load_proverbs()
+    if 0 <= index < len(data):
+        data[index]["votes"] = data[index].get("votes", 0) + 1
+        with open(PROVERB_FILE, "w", encoding="utf-8") as f:
+            json.dump(data, f, ensure_ascii=False, indent=2)
