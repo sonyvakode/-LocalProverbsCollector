8a5f172a535176c3904d5e106b16eff10dd025f1
sonyvakode <sonyvakode648@gmail.com>
2025-07-31 22:22:11 +0530

    Update vote.py

--- utils/vote.py
+++ utils/vote.py
@@ -1,14 +1,11 @@
-from .core import load_proverbs, save_proverb
-import os
+from .core import load_proverbs, DATA_FILE
 import json
 
-PROVERB_FILE = "data/proverbs.json"
-
-def vote_proverb(proverb_text):
-    proverbs = load_proverbs()
-    for p in proverbs:
-        if p["proverb"] == proverb_text:
-            p["votes"] = p.get("votes", 0) + 1
+def increment_vote(proverb_text):
+    data = load_proverbs()
+    for item in data:
+        if item["proverb"] == proverb_text:
+            item["votes"] += 1
             break
-    with open(PROVERB_FILE, "w", encoding="utf-8") as f:
-        json.dump(proverbs, f, ensure_ascii=False, indent=2)
+    with open(DATA_FILE, "w", encoding="utf-8") as f:
+        json.dump(data, f, ensure_ascii=False, indent=2)
