be663eb52d5805d5926615ed079bd4de269ef0a1
sonyvakode <sonyvakode648@gmail.com>
2025-07-31 19:37:07 +0530

    Update language.py

--- utils/language.py
+++ utils/language.py
@@ -1,20 +1,4 @@
-# utils/language.py
-
 def get_supported_languages():
-    """
-    Returns a list of supported languages for the app.
-    These should match gTTS and GoogleTranslator language codes.
-    """
     return [
-        "en",  # English
-        "hi",  # Hindi
-        "ta",  # Tamil
-        "te",  # Telugu
-        "ml",  # Malayalam
-        "kn",  # Kannada
-        "gu",  # Gujarati
-        "mr",  # Marathi
-        "bn",  # Bengali
-        "pa",  # Punjabi
-        "ur",  # Urdu
+        "en", "hi", "ta", "te", "ml", "kn", "gu", "mr", "bn", "pa", "ur"
     ]
