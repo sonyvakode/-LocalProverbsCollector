059b70dfc5b4f8860773b96ed56378ef1fb00399
sonyvakode <sonyvakode648@gmail.com>
2025-07-31 20:14:06 +0530

    Update vote.py

--- utils/vote.py
+++ utils/vote.py
@@ -1,6 +1,12 @@
 # utils/vote.py
 
 import random
+import sys
+import os
+
+# Add parent directory to sys.path to allow absolute import of core
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
 from utils.core import load_proverbs
 
 def get_random():
