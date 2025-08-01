066b609d5fee9204fc02d57688903810d38be276
sonyvakode <sonyvakode648@gmail.com>
2025-08-01 19:45:51 +0530

    Update vote.py

--- utils/vote.py
+++ utils/vote.py
@@ -1,19 +1,23 @@
 import json
+import os
 
-DATA_FILE = "data/proverbs.json"
+VOTE_FILE = "data/votes.json"
+
+def _load_votes():
+    if not os.path.exists(VOTE_FILE):
+        return {}
+    with open(VOTE_FILE, "r", encoding="utf-8") as f:
+        return json.load(f)
+
+def _save_votes(votes):
+    os.makedirs("data", exist_ok=True)
+    with open(VOTE_FILE, "w", encoding="utf-8") as f:
+        json.dump(votes, f, indent=2)
+
+def like_proverb(proverb):
+    votes = _load_votes()
+    votes[proverb] = votes.get(proverb, 0) + 1
+    _save_votes(votes)
 
 def get_all():
-    try:
-        with open(DATA_FILE, "r", encoding="utf-8") as f:
-            return json.load(f)
-    except:
-        return []
-
-def like_proverb(target_proverb):
-    data = get_all()
-    for item in data:
-        if item["proverb"] == target_proverb:
-            item["likes"] = item.get("likes", 0) + 1
-            break
-    with open(DATA_FILE, "w", encoding="utf-8") as f:
-        json.dump(data, f, indent=4)
+    return _load_votes()
