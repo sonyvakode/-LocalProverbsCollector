b648ef6e7a79069a5f9a26a51e4e8a5dcd9d2bc0
sonyvakode <sonyvakode648@gmail.com>
2025-08-02 23:13:33 +0530

    Update vote.py

--- utils/vote.py
+++ utils/vote.py
@@ -1,30 +1,12 @@
-from .core import load_proverbs
-from pathlib import Path
+from .core import load_data, save_data
 
-DATA_FILE = "data/proverbs.txt"
-
-def _write_all(proverbs):
-    with open(DATA_FILE, "w", encoding="utf-8") as f:
-        for p in proverbs:
-            f.write(f"{p[0]}|{p[1]}|{p[2]}|{p[3]}|{p[4]}\n")
+def get_all():
+    return load_data()
 
 def like_proverb(text):
-    proverbs = load_proverbs()
-    for i, p in enumerate(proverbs):
-        if p[0] == text:
-            proverbs[i] = (p[0], p[1], p[2], p[3], p[4] + 1)
+    data = load_data()
+    for p in data:
+        if p["text"] == text:
+            p["likes"] += 1
             break
-    _write_all(proverbs)
-
-def increment_view(text):
-    proverbs = load_proverbs()
-    for i, p in enumerate(proverbs):
-        if p[0] == text:
-            proverbs[i] = (p[0], p[1], p[2], p[3] + 1, p[4])
-            break
-    _write_all(proverbs)
-
-def get_top_liked(n=5):
-    proverbs = load_proverbs()
-    sorted_proverbs = sorted(proverbs, key=lambda x: x[4], reverse=True)
-    return [(p[0], p[4]) for p in sorted_proverbs[:n]]
+    save_data(data)
