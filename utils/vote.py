48e17ec57c7eaee9eee2726c392a625dab751cda
sonyvakode <sonyvakode648@gmail.com>
2025-08-03 19:40:45 +0530

    Update vote.py

--- utils/vote.py
+++ utils/vote.py
@@ -1,22 +1,40 @@
-import os
 import json
+import os
 
-VOTE_FILE = "data/votes.json"
+DATA_PATH = "data/stats.json"
 
-def _load_votes():
-    if not os.path.exists(VOTE_FILE):
-        return {}
-    with open(VOTE_FILE, "r", encoding="utf-8") as f:
-        return json.load(f)
+def _load_data():
+    if not os.path.exists(DATA_PATH):
+        return []
+    with open(DATA_PATH, "r", encoding="utf-8") as f:
+        try:
+            return json.load(f)
+        except json.JSONDecodeError:
+            return []
 
-def _save_votes(votes):
-    with open(VOTE_FILE, "w", encoding="utf-8") as f:
-        json.dump(votes, f)
+def _save_data(data):
+    with open(DATA_PATH, "w", encoding="utf-8") as f:
+        json.dump(data, f, ensure_ascii=False, indent=2)
 
-def like_proverb(proverb):
-    votes = _load_votes()
-    votes[proverb] = votes.get(proverb, 0) + 1
-    _save_votes(votes)
+def like_proverb(proverb_text):
+    data = _load_data()
+    for item in data:
+        if item["proverb"] == proverb_text:
+            item["likes"] += 1
+            break
+    else:
+        data.append({"proverb": proverb_text, "likes": 1, "views": 1})
+    _save_data(data)
+
+def view_proverb(proverb_text):
+    data = _load_data()
+    for item in data:
+        if item["proverb"] == proverb_text:
+            item["views"] += 1
+            break
+    else:
+        data.append({"proverb": proverb_text, "likes": 0, "views": 1})
+    _save_data(data)
 
 def get_all():
-    return _load_votes()
+    return _load_data()
