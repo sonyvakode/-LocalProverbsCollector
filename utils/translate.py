3bc79a8cecfff7bcac02123eba958edef472a4f1
sonyvakode <sonyvakode648@gmail.com>
2025-08-03 19:41:02 +0530

    Update translate.py

--- utils/translate.py
+++ utils/translate.py
@@ -1,10 +1,8 @@
 from deep_translator import GoogleTranslator
-from .language import LANGUAGE_NAMES_TO_CODES
 
 def translate_text(text, target_lang):
     try:
-        lang_code = LANGUAGE_NAMES_TO_CODES.get(target_lang.lower(), "en")
-        translated = GoogleTranslator(target=lang_code).translate(text)
+        translated = GoogleTranslator(source='auto', target=target_lang).translate(text)
         return translated
     except Exception as e:
-        return f"⚠️ Translation error: {str(e)}"
+        return f"Translation failed: {str(e)}"
