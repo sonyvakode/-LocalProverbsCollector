d82f211d61a067293274d14554312e0895da11e6
sonyvakode <sonyvakode648@gmail.com>
2025-08-03 17:29:53 +0530

    Update language.py

--- utils/language.py
+++ utils/language.py
@@ -1,16 +1,20 @@
-# utils/language.py
+LANGUAGES = {
+    "English": "en",
+    "Hindi": "hi",
+    "Tamil": "ta",
+    "Telugu": "te",
+    "Kannada": "kn",
+    "Gujarati": "gu",
+    "Marathi": "mr",
+    "Bengali": "bn",
+    "Malayalam": "ml",
+    "Punjabi": "pa",
+    "Urdu": "ur",
+    "Odia": "or"
+}
 
-def get_supported_languages():
-    return {
-        "English": "en",
-        "Hindi": "hi",
-        "Telugu": "te",
-        "Kannada": "kn",
-        "Tamil": "ta",
-        "Gujarati": "gu",
-        "Bengali": "bn",
-        "Marathi": "mr",
-        "Punjabi": "pa",
-        "Malayalam": "ml",
-        "Urdu": "ur"
-    }
+def get_language_code(language_name):
+    return LANGUAGES.get(language_name, "en")
+
+def get_all_languages():
+    return list(LANGUAGES.keys())
